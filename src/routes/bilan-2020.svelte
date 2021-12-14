59f8a6b571524eabf29dee00f75b61dfed4704b0
Julien Glotain <jglotain@gmail.com>
2021-12-14 21:49:42 +0100

    fix bilan pdf path

--- src/routes/bilan-2020.svelte
+++ src/routes/bilan-2020.svelte
@@ -3,7 +3,7 @@
 </svelte:head>
 <div class="container mx-auto w-4/5 h-5/6 mt-1 mb-1">
   <iframe
-    src="http://velovolenantes.fr/data/velovolenantes_bilan_2020_CCBYSA3.0FR.pdf"
+    src="/data/velovolenantes_bilan_2020_CCBYSA3.0FR.pdf"
     title="PDF Bilan 2020"
     class="w-full h-full"
   />
